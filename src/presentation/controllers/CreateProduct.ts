f48b7b182df215b6e07353244d784d2ddfecf7a1
Dimas Paiva <dimas.paiva@luizalabs.com>
2022-03-20 02:29:16 -0300

    feat: ensure return error when product description is missing

--- src/presentation/controllers/CreateProduct.ts
+++ src/presentation/controllers/CreateProduct.ts
@@ -5,9 +5,16 @@ import { HTTPRequest, HTTPResponse } from '../protocols/http'
 export class CreateProductController implements Controller {
   async handle (httpRequest: HTTPRequest): Promise<HTTPResponse> {
 
-    return {
-      statusCode: 400,
-      body: new MissingParamError('name')
+    if (!httpRequest.body.product.name) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('name')
+      }
+    } else if (!httpRequest.body.product.description) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('description')
+      }
     }
   }
 }
