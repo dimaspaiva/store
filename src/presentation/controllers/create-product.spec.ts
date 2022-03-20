fe0bb67a4f59bd95c79f06188ac665236daaa3b6
Dimas Paiva <dimas.paiva@luizalabs.com>
2022-03-20 02:26:18 -0300

    refactor: use factory to create sut

--- src/presentation/controllers/create-product.spec.ts
+++ src/presentation/controllers/create-product.spec.ts
@@ -1,13 +1,25 @@
 import { CreateProductController } from './CreateProduct'
 import { MissingParamError } from '../errors/MissingParam'
 
+const testProduct = {
+  name: 'Product Name',
+  amount: 10,
+  description: 'Product description'
+}
+
+
+const makeSut = () => {
+  const sut = new CreateProductController()
+
+  return {
+    sut
+  }
+}
+
 describe('Create Product', () => {
   it('Should return 400 if product has no name', async () => {
-    const sut = new CreateProductController()
-    const product = {
-      amount: 10,
-      description: 'Product description'
-    }
+    const { sut } = makeSut()
+    const {name, ...product} = testProduct
 
     const response = await sut.handle({ body: { product } })
     expect(response.statusCode).toBe(400)
